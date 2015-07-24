6292bdfb54d9617da0203d527b211f98e4ab5730
Steve Kemp <steve@steve.org.uk>
2015-07-24 11:32:37 +0000

    Updated to set more domains.

--- .lumail.d/from.lua
+++ .lumail.d/from.lua
@@ -7,6 +7,10 @@
 --
 function on_folder_selection( folder )
 
+   if ( ( folder == nil ) or ( folder == "" ) ) then
+      return
+   end
+
    --
    -- Change the email address we use depending on our folder.
    --
@@ -20,10 +24,15 @@ function on_folder_selection( folder )
       from("Steve <steve@edinburgh-portraits.com>");
    elseif ( string.find( folder, "dns-api.com" , 1, true )  ) then
       from("Steve Kemp <steve@dns-api.com>");
+   elseif ( string.find( folder, "spare.io" , 1, true )  ) then
+      from("Steve <steve@spare.io>");
    elseif ( string.find( folder, "dhcp.io" , 1, true )  ) then
-      from("Steve <steve@dhcp.io>");
+      from("Steve Kemp <steve@dhcp.io>");
+   elseif ( string.find( folder, "leith-flat.com" , 1, true )  ) then
+      from("Steve Kemp <steve@leith-flat.com>");
+   elseif ( string.find( folder, "interesting.photography" , 1, true )  ) then
+      from("Steve Kemp <steve@interesting.photography");
    else
       from( default_email );
    end
 end
-
