b10a75bb0b65cd121e1e7ea5ae4ccff5dec77be2
Steve Kemp <steve@steve.org.uk>
2015-11-28 11:20:16 +0200

    Moved keybindings into init.el - temporary.

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -275,3 +275,47 @@ If FEATURE can't be loaded, don't execute BODY."
 ;; Copying in emacs should allow pasting into gnome-terminal, etc.
 ;;
 (setq x-select-enable-clipboard t)
+
+
+;;
+;; M-g = goto-line
+;;
+(global-set-key "\M-g" 'goto-line)
+
+;;
+;;  Search by regexp
+;;
+(global-set-key "\C-s" 'isearch-forward-regexp)
+(global-set-key "\C-r" 'isearch-backward-regexp)
+
+;;
+;; C-x k == Kill current buffer, without prompting.
+;;
+(global-set-key "\C-xk"
+		'(lambda ()
+                   (interactive)
+                   (if (and (fboundp 'gnuserv-buffer-p) (gnuserv-buffer-p (current-buffer)))
+                       (gnuserv-buffer-done (current-buffer))
+                     (kill-buffer (current-buffer)))))
+
+
+;;
+;; Prevent accidentally killing emacs.
+;;
+(global-set-key [(control x) (control c)]
+		'(lambda ()
+		   (interactive)
+		   (if (y-or-n-p-with-timeout "Do you really want to exit Emacs ? " 10 nil)
+                       (progn
+                         (if window-system
+                             (progn
+                               (uptime)
+                               (sleep-for 1)))
+                         (save-buffers-kill-emacs)))
+                     (message "emacs quit aborted")))
+
+
+
+
+
+(provide 'skx-keybindings)
