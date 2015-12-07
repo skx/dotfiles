f8fc0238c0007cc96cbc5de8f47fe8e0875fe013
Steve Kemp <steve@steve.org.uk>
2015-12-07 16:12:03 +0000

    Added to repository.

---
+++ .lumail2/ssh.steve.org.uk.lua
@@ -0,0 +1,40 @@
+--
+-- Show only maildirs which have received mail today
+-- by default.
+--
+Config:set( "maildir.limit", "today" )
+
+--
+-- Show only mail received today, by default
+--
+Config:set( "index.limit", "today" )
+
+
+--
+-- Setup our editor
+--
+Config:set( "global.editor", "vim  +/^$ ++1 '+set tw=72'" )
+
+--
+-- Setup our default From: address.
+--
+Config:set( "global.sender", "Steve Kemp <steve@steve.org.uk>" )
+
+--
+-- Setup folder-based addresses.
+--
+-- NOTE: `%` is used to escape special characters in Lua's regular
+-- expression library.  As you can imagine this is horrid, and precisely
+-- why we ported PCRE ;)
+--
+folder_from = {
+   ['debian%-administration%.'] = "Steve Kemp <steve@debian-administration.org>",
+   ['dns%-api.com'] = "Steve Kemp <steve@dns-api.com>",
+   ['dhcp.io'] = "Steve Kemp <steve@dhcp.io>",
+}
+
+
+--
+-- Unread messages/maildirs are drawn in red.
+--
+Config:set( "colour.unread", "red" )
