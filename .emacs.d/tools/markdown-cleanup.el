6a8862c99df3a9d05c68e72f96d6370cd7b51400
Steve Kemp <steve@steve.org.uk>
2023-04-20 21:16:50 +0300

    Renamed.

--- .emacs.d/tools/markdown-cleanup.el
+++ .emacs.d/tools/markdown-cleanup.el
@@ -1,4 +1,4 @@
-;;; markdown-cleanup.el - Cleanup Spacing Between Markdown/Org headers
+;;; vertical-space-cleanup.el - Cleanup Spacing Between Markdown/Org headers
 
 
 ;; This package is designed to ensure that documents have a consistent number of
@@ -17,9 +17,9 @@
 ;;
 ;; Here we see there is a difference in the vertical whitespace between the
 ;; two second-level headers "## Authors" and "## References".  Using this
-;; package the `markdown-cleanup' command can be invoked and will reformat
-;; all headers to have an identical number of newlines between the header and
-;; any previous content.
+;; package the `vertical-space-cleanup' command can be invoked and will
+;; reformat all headers to have an identical number of newlines between
+;; the header and any previous content.
 ;;
 ;; The configuration of the spacing depends upon the header-level.  In my
 ;; personal configuration:
@@ -30,17 +30,12 @@
 ;;  ### Level 4 heading  - one newlines before it
 ;;
 ;; The choice is made by looking at the Nth element of the list with name
-;; `markdown-cleanup-indent-md' - similar list exists for org-mode - after
+;; `vertical-space-cleanup-md' - similar list exists for org-mode - after
 ;; removing one.
 ;;
 ;; BUGS:
 ;;
 ;; - None known, please report if you see something wrong.
-;;
-;;
-;; TODO:
-;;
-;;  - Better name, now we support org-mode too.
 ;;
 
 
@@ -51,7 +46,7 @@
 ;; The following two lists handle that.   We should probably use an alist
 ;; to get the values.
 ;;
-(defvar markdown-cleanup-indent-md
+(defvar vertical-space-cleanup-md
   (list
    "\n\n\n\n"  ;; level 1 - 4 newlines before it
    "\n\n\n"    ;; level 2 - 3 newlines before it
@@ -59,11 +54,10 @@
    "\n"        ;; level 4 - 1 newlines before it
    ))          ;; other levels get zero.
 
-(defvar markdown-cleanup-indent-org
+(defvar vertical-space-cleanup-org
   (list
    "\n"    ;; level 1 - 1 newline
    ))      ;; other levels get zero.
-
 
 
 (defun count-chars (sep str)
@@ -71,37 +65,35 @@
    (let ((qsep (regexp-quote sep)))
      (- (length (split-string str qsep)) 1)))
 
-(defun markdown-cleanup-header-regexp ()
+(defun vertical-space-cleanup-header-regexp ()
   "Return the appropriate regexp for finding headers.
 
 This supports `markdown-mode', and `org-mode', along with anything derived from either."
-  (interactive)
   (cond
    ((derived-mode-p 'markdown-mode) "^\n*#+")
    ((derived-mode-p 'org-mode)      "^\n*\\*+")))
 
-(defun markdown-cleanup-get-header-level (header)
+(defun vertical-space-cleanup-get-header-level (header)
   "Return the header depth level.
 
 This supports `markdown-mode', and `org-mode', along with anything derived from either."
   (interactive)
   (cond
-   ((derived-mode-p 'markdown-mode) (count-chars "#" header))
-   ((derived-mode-p 'org-mode)      (count-chars "*" header))))
+   ((derived-mode-p 'markdown-mode)   (count-chars "#" header))
+   ((derived-mode-p 'org-mode)        (count-chars "*" header))))
 
-
-(defun markdown-cleanup-get-newlines (level)
+(defun vertical-space-cleanup-get-newlines (level)
   "Return the string of newlines for the given mode.
 
 This is based upon the header level, and is configurable via the two lists
-`markdown-cleanup-indent-md' and `markdown-cleanup-indent-org'."
+`vertical-space-cleanup-md' and `vertical-space-cleanup-org'."
   (cond
-   ((derived-mode-p 'markdown-mode) (nth (- level 1) markdown-cleanup-indent-md))
-   ((derived-mode-p 'org-mode)      (nth (- level 1) markdown-cleanup-indent-org))))
+   ((derived-mode-p 'markdown-mode) (nth (- level 1)   vertical-space-cleanup-md))
+   ((derived-mode-p 'org-mode)      (nth (- level 1)   vertical-space-cleanup-org))))
 
 
 
-(defun markdown-cleanup ()
+(defun vertical-space-cleanup ()
   "Ensure there is consistent whitespace between headers and previous sections.
 
 We iterate over the buffer and start by removing all newlines between headers,
@@ -116,7 +108,7 @@ any modes derived from either of those."
     (goto-char (point-min))
 
     ;; For each header
-    (while (re-search-forward (markdown-cleanup-header-regexp) nil t)
+    (while (re-search-forward (vertical-space-cleanup-header-regexp) nil t)
       ;; get the header, and count the depth of it.
       (let* ((match (match-string 0))
              (lines (count-chars "\n" match)))
@@ -134,23 +126,23 @@ any modes derived from either of those."
 
     ;; now add new lines
     ;; for each header
-    (while (re-search-forward (markdown-cleanup-header-regexp) nil t)
+    (while (re-search-forward (vertical-space-cleanup-header-regexp) nil t)
       ;; count the depth of it
       (let* ((match (match-string 0))
-             (level (markdown-cleanup-get-header-level match)))
+             (level (vertical-space-cleanup-get-header-level match)))
         ;; move to start of line
         (beginning-of-line)
 
         ;; insert newlines, based on header level
-        (if (markdown-cleanup-get-newlines level)
-            (insert (markdown-cleanup-get-newlines level)))
+        (if (vertical-space-cleanup-get-newlines level)
+            (insert (vertical-space-cleanup-get-newlines level)))
 
         ;; forward to avoid infinite loops
         (end-of-line))))
-    (markdown-cleanup-kill-leading-whitespace))
+    (vertical-space-cleanup-kill-leading-whitespace))
 
 
-(defun markdown-cleanup-kill-leading-whitespace ()
+(defun vertical-space-cleanup-kill-leading-whitespace ()
   "Remove whitespace at the start of the file.
 
 This is largely required because the markdown setup will add four newlines before
@@ -162,12 +154,6 @@ a level-one header, by default, and those typically occur at the start of the do
     (delete-region (point)
                    (+ (save-excursion (skip-chars-forward " \n"))
                       (point)))))
-
-;; Install this package automatically to both markdown-mode and org-mode files.
-(add-hook 'markdown-mode-hook
-          (lambda () (add-hook 'before-save-hook #'markdown-cleanup t t)))
-(add-hook 'org-mode-hook
-          (lambda () (add-hook 'before-save-hook #'markdown-cleanup t t)))
 
 ;; Provide the package
 (provide 'markdown-cleanup)
