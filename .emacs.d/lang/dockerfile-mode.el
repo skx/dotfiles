dbde82cefdd1e69ee811ccaba99bcca3dc3d0733
Steve Kemp <steve@steve.org.uk>
2024-02-22 15:01:58 +0200

    resynced from upstream source

--- .emacs.d/lang/dockerfile-mode.el
+++ .emacs.d/lang/dockerfile-mode.el
@@ -160,11 +160,8 @@ by `dockerfile-enable-auto-indent'."
              '(font-lock-comment-delimiter-face font-lock-keyword-face))
      (save-excursion
        (beginning-of-line)
-       (skip-chars-forward "[ \t]" (point-at-eol))
-       (unless (equal (point) (point-at-eol)) ; Ignore empty lines.
-         ;; Delete existing whitespace.
-         (delete-char (- (point-at-bol) (point)))
-         (indent-to dockerfile-indent-offset))))))
+       (unless (looking-at-p "\\s-*$") ; Ignore empty lines.
+         (indent-line-to dockerfile-indent-offset))))))
 
 (defun dockerfile-build-arg-string ()
   "Create a --build-arg string for each element in `dockerfile-build-args'."
