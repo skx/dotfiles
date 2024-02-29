59903b3c2e45c1c30038f5dcd5f4ca8b5a7aa3fa
Steve Kemp <steve@steve.org.uk>
2024-02-29 09:21:30 +0200

    resync

--- .emacs.d/tools/save-check.el
+++ .emacs.d/tools/save-check.el
@@ -7,25 +7,30 @@
       '(
 
         (:mode json-mode
-         :exec "sysbox validate-json %s"
+         :exec "sysbox validate-json %f"
          :cond (executable-find "sysbox"))
 
         (:mode nxml-mode
-         :exec "sysbox validate-xml %s"
+         :exec "sysbox validate-xml %f"
          :cond (executable-find "sysbox"))
 
         ;; This avoids creating .pyc files, which would happen if we had
         ;; used the more natural/obvious "python3 -m py_compile %s" approach
         (:mode python-mode
-         :exec "python3 -c 'import ast; ast.parse(open(\"%s\").read())'"
+         :exec "python3 -c 'import ast; ast.parse(open(\"%f\").read())'"
          :cond (executable-find "python3"))
 
         (:mode sh-mode
-         :exec "shellcheck %s"
+         :exec "shellcheck %f"
          :cond (executable-find "shellcheck"))
 
+        (:mode terraform-mode
+         :exec "tflint --no-color --chdir %d"
+         :cond (executable-find "tflint")
+         :path t)
+
         (:mode yaml-mode
-         :exec "sysbox validate-yaml %s"
+         :exec "sysbox validate-yaml %f"
          :cond (executable-find "sysbox"))
        ))
 
@@ -34,12 +39,13 @@
   "Run the save-check if the current buffer has a configured command.
 
 The commands are contained within the list `save-check-config', and those
-commands will have '%s' replaced with the path to the saved file."
+commands will have parameters replaced appropriately by `save-check-run-command'."
   (interactive)
   (mapc #'(lambda (entry)
             (let ((exec (plist-get entry :exec))
                   (cnd (plist-get entry :cond))
                   (mode (plist-get entry :mode))
+                  (path (plist-get entry :path))
                   (run nil))
 
               ;; If there is a condition set
@@ -49,6 +55,8 @@ commands will have '%s' replaced with the path to the saved file."
                 (setq run t) ;; otherwise, no condition set, we run
                 )
 
+              ;; If we're to run, and the mode is either a) the exact mode, or b) derived from that mode
+              ;; then run.
               (if (and run (or (derived-mode-p mode) (eq major-mode  mode)))
                   (save-check-run-command exec))))
         save-check-config)
@@ -56,27 +64,40 @@ commands will have '%s' replaced with the path to the saved file."
 
 
 (defun save-check-run-command(cmd)
-  "Execute the specified command, with the name of the buffers file as an argument.
+  "Execute the specified command, expanding parameters as expected:
 
-If the command exists with a zero-return code then nothing happens, otherwise the output will be shown."
-  (interactive)
+`%f' will be replaced with the path to the file.
+`%d' will be replaced with the directory containing the file.
+
+If the command exists with a zero-return code then nothing happens, otherwise the output will be shown in a popup-buffer."
+
+  ;; get, and kill, any existing buffer.
   (with-current-buffer (get-buffer-create "*save-check*")
     (kill-buffer))
-  (let ((buffer (get-buffer-create "*save-check*"))
-        (ret nil))
 
-    ;; empty the buffer
-    (with-current-buffer buffer
-                 (erase-buffer))
+  ;; setup variables
+  (let ((buffer (get-buffer-create "*save-check*"))
+        (exec   cmd)
+        (ret    nil))
+
+    ;; Expand the string we're to execute.
+    ;;  %f -> file
+    (setq exec (string-replace "%f" buffer-file-name exec))
+    ;;  %d -> directory
+    (setq exec (string-replace "%d" (file-name-directory buffer-file-name) exec))
 
     ;; call the process
-    (setq ret (call-process-shell-command (format cmd buffer-file-name) nil buffer nil))
-  (if (= 0 ret)
-      (kill-buffer buffer)
-    (progn
-      (pop-to-buffer buffer)
-      (special-mode)
-      ))))
+    (setq ret (call-process-shell-command exec nil buffer nil))
+
+    ;; if the return code was OK, kill the results.
+    ;; otherwise pop to the buffer, and set it to be "special mode"
+    ;; which has suitable keybindings.
+    (if (= 0 ret)
+        (kill-buffer buffer)
+      (progn
+        (pop-to-buffer buffer)
+        (special-mode)
+        ))))
 
 
 ;; Add the hook
