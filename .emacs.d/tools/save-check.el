971d8010356a52a7e60a884524aae6befb1a2134
Steve Kemp <steve@steve.org.uk>
2024-03-02 08:28:04 +0200

    Resync from upstream

--- .emacs.d/tools/save-check.el
+++ .emacs.d/tools/save-check.el
@@ -3,7 +3,7 @@
 
 
 ;; The default linters that we ship with.
-(setq save-check-config
+(defvar save-check-config
       '(
 
         (:mode dockerfile-mode
@@ -30,25 +30,81 @@
 
         (:mode terraform-mode
          :exec "tflint --no-color --chdir %d"
-         :cond (executable-find "tflint")
-         :path t)
+         :cond (executable-find "tflint"))
 
         (:mode yaml-mode
          :exec "sysbox validate-yaml %f"
          :cond (executable-find "sysbox"))
-       ))
+        )
+      "This is the configuration for `save-check'.
+
+Each time a file is saved it's mode will be compared with each item in this list.
+If the mode matches either the command specified in `:exec' will be executed, or the
+lisp expression specified in `:eval' will be evaluated.")
+
+
+(defvar save-check-show-eval
+  nil
+  "If this variable is set then we show the output of lisp expressions evaluated with the `:eval' key.
+
+Otherwise they are discarded.")
+
+
+(defvar save-check-buffer-name
+  "*save-check*"
+  "The name of the buffer which is used to show failing results, if any.")
+
+
+
+
+
+(defun save-check-config-ok-p ()
+  "Test that the configuration list `save-check-config' is well-formed.
+
+For each entry in the list we ensure there is a `mode' key, along with
+either `:exec' or `:eval'.
+
+If either sanity-check fails then an error is raised."
+  (mapc #'(lambda (entry)
+            (let ((exec (plist-get entry :exec))
+                  (lisp (plist-get entry :eval))
+                  (mode (plist-get entry :mode))
+                  )
+
+              ;; missing mode? error
+              (if mode
+                  ""
+                (error "entry in save-check-config is missing :mode key"))
+
+              ;; missing :exec and :lisp?  error
+              (if (or exec lisp)
+                  ""
+                 (error "save-check config is missing :exec and :eval for mode %s" mode))))
+        save-check-config)
+  t)
 
 
 (defun save-check-run()
-  "Run the save-check if the current buffer has a configured command.
+  "The `save-check-run' function is added to the global `after-save-hook'.
+If the current `major-mode' matches an entry in `save-check-config' then
+a check will be carried out.
 
-The commands are contained within the list `save-check-config', and those
-commands will have parameters replaced appropriately by `save-check-run-command'."
+   A check is typically called by executing an external process, and testing
+the status code of that call, however it is also possible to call an arbitrary
+lisp expression.
+
+  If a command is listed in the `:exec' key it will be invoked via `save-check-run-command',
+if a lisp expression is given under the `:eval' key it will be invoked by `save-check-run-lisp'."
   (interactive)
+
+  ;; Test the config is OK before doing anything else
+  (save-check-config-ok-p)
+
   (mapc #'(lambda (entry)
             (let ((exec (plist-get entry :exec))
                   (cnd (plist-get entry :cond))
                   (mode (plist-get entry :mode))
+                  (lisp (plist-get entry :eval))
                   (path (plist-get entry :path))
                   (run nil))
 
@@ -60,11 +116,46 @@ commands will have parameters replaced appropriately by `save-check-run-command'
                 )
 
               ;; If we're to run, and the mode is either a) the exact mode, or b) derived from that mode
-              ;; then run.
+              ;; then do so.
+              ;; If we have a lisp expression, run that.
+              ;; If we have a command, run that.
+              ;; This allows both, or neither, to operate
               (if (and run (or (derived-mode-p mode) (eq major-mode  mode)))
-                  (save-check-run-command exec))))
+                  (progn
+                    (if lisp (save-check-run-lisp lisp))
+                    (if exec (save-check-run-command exec))))))
         save-check-config)
   )
+
+
+
+(defun save-check-run-lisp(expr)
+  "Execute the specified expression.
+
+If the variable `save-check-show-eval' then the output of the specified expression will be shown in a popup-buffer, unless it is nil.  Otherwise any output will be discarded."
+
+  ;; get, and kill, any existing buffer.
+  (with-current-buffer (get-buffer-create save-check-buffer-name)
+    (kill-buffer))
+
+  ;; setup variables
+  (let ((buffer (get-buffer-create save-check-buffer-name))
+        (ret    nil))
+
+    ;; evaluate the expression, and get the result
+    (setq ret (eval expr))
+
+    ;; if there was no output, kill it
+    (if (not ret)
+        (kill-buffer buffer)
+      (if save-check-show-eval
+        ;; ok output was made, show it.
+        (progn
+          (pop-to-buffer buffer)
+          (insert ret)
+          (special-mode)
+        )))))
+
 
 
 (defun save-check-run-command(cmd)
@@ -73,14 +164,14 @@ commands will have parameters replaced appropriately by `save-check-run-command'
 `%f' will be replaced with the path to the file.
 `%d' will be replaced with the directory containing the file.
 
-If the command exists with a zero-return code then nothing happens, otherwise the output will be shown in a popup-buffer."
+If the command exits with a zero-return code then nothing happens, otherwise the output will be shown in a popup-buffer."
 
   ;; get, and kill, any existing buffer.
-  (with-current-buffer (get-buffer-create "*save-check*")
+  (with-current-buffer (get-buffer-create save-check-buffer-name)
     (kill-buffer))
 
   ;; setup variables
-  (let ((buffer (get-buffer-create "*save-check*"))
+  (let ((buffer (get-buffer-create save-check-buffer-name))
         (exec   cmd)
         (ret    nil))
 
