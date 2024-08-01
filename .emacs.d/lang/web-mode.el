f943759ad16c2d70daa3a8d2130f96a523466397
Steve Kemp <steve@steve.org.uk>
2024-08-01 12:06:19 +0300

    Resynced packages from upstream

--- .emacs.d/lang/web-mode.el
+++ .emacs.d/lang/web-mode.el
@@ -1385,7 +1385,7 @@ For example,
                  ("ifequal"    . "{% ifequal | %}\n\n{% endifequal %}")
                  ("ifnotequal" . "{% ifnotequal | %}\n\n{% endifnotequal %}")
                  ("js"         . "{% javascript | %}\n\n{% endjavascript %}")
-                 ("schema"     . "{% javascript | %}\n\n{% endschema %}")
+                 ("schema"     . "{% schema | %}\n\n{% endschema %}")
                  ("safe"       . "{% safe | %}\n\n{% endsafe %}")))
     ("mako" . (("if"        . "% if |:\n% endif")
                ("for"       . "% for | in :\n% endfor")
@@ -4294,6 +4294,7 @@ Also return non-nil if it is the command `self-insert-command' is remapped to."
     (goto-char pos)
     (forward-char)
     (skip-chars-forward "a-zA-Z0-9_-")
+    (skip-chars-forward " ")
     (when (eq (char-after) ?\()
       (setq regexp "[\"'()]"
             inc 0)
